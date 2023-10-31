f249e92e1779fa6850440017fc02881a94406e98
VladyslavPC <volodkatraktorist@gmail.com>
2023-10-31 17:30:01 +0200

    lab4

--- template/src/main/java/com/lab111/labwork4/Point.java
+++ template/src/main/java/com/lab111/labwork4/Point.java
@@ -1,5 +1,8 @@
 package main.java.com.lab111.labwork4;
 
 public interface Point {
+    void setX(int x);
+
+    void setY(int y);
 
 }
--- template/src/main/java/com/lab111/labwork4/PointPrimitive.java
+++ template/src/main/java/com/lab111/labwork4/PointPrimitive.java
@@ -1,19 +1,22 @@
 package main.java.com.lab111.labwork4;
 
-public class PointPrimitive implements Point{
+public class PointPrimitive implements Point {
     private int X;
     private int Y;
 
-    public PointPrimitive(int X, int Y){
+    public PointPrimitive(int X, int Y) {
         this.X = X;
         this.Y = Y;
     }
 
+    @Override
     public void setX(int x) {
         X = x;
     }
 
+    @Override
     public void setY(int y) {
         Y = y;
     }
 }
+
