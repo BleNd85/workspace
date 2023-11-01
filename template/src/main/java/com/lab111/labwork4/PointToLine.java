e6be746f1acff9719ff20453abc23b86d523e3f5
VladyslavPC <volodkatraktorist@gmail.com>
2023-11-01 14:59:25 +0200

    lab4R

--- template/src/main/java/com/lab111/labwork4/PointToLine.java
+++ template/src/main/java/com/lab111/labwork4/PointToLine.java
@@ -1,12 +1,13 @@
 package main.java.com.lab111.labwork4;
 
 public class PointToLine implements GraphicObject {
+
+    private Point startPoint;
+    private Point endPoint;
     private double x1;
     private double y1;
     private double x2;
     private double y2;
-    private Point startPoint;
-    private Point endPoint;
 
     public PointToLine(Point startPoint, Point endPoint) {
         this.startPoint = startPoint;
@@ -19,6 +20,6 @@ public class PointToLine implements GraphicObject {
         y1 = startPoint.getY();
         x2 = endPoint.getX();
         y2 = endPoint.getY();
-        System.out.println(x1 + x2 + y1 + y2);
+        System.out.println();
     }
 }
